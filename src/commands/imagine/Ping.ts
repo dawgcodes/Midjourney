fa3ac29e5f0639963ff2fb052e9a5458250a7b63
TwoDawg <ianblackdavid@gmail.com>
2024-04-28 08:30:54 +0200

    Update Ping.ts

--- src/commands/imagine/Ping.ts
+++ src/commands/imagine/Ping.ts
@@ -1,17 +1,21 @@
-import { CommandInteraction } from "discord.js";
-import { Command, Bot } from "../../structures/index.js";
+import { CommandInteraction } from 'discord.js';
 
+import { Bot, Command } from '../../structures/index.js';
 
 export default class Ping extends Command {
     constructor(client: Bot) {
         super(client, {
             name: 'ping',
+            nameLocalizations: {
+                fr: 'ping',
+            },
             description: {
                 content: '🏓 | Get the bot latency',
                 usage: 'ping',
-                examples: [
-                    'ping',
-                ],
+                examples: ['ping'],
+            },
+            descriptionLocalizations: {
+                fr: '🏓 | Obtiens la latence du bot.',
             },
             category: 'general',
             permissions: {
@@ -23,11 +27,13 @@ export default class Ping extends Command {
             options: [],
         });
     }
-    async run(client: Bot, interaction: CommandInteraction) {
+    async run(client: Bot, interaction: CommandInteraction): Promise<void> {
         await interaction.deferReply({ fetchReply: true });
-        await interaction.editReply({ content: `Pinging...` }).then(async (msg) => {
+        await interaction.editReply({ content: `Pinging...` }).then(async msg => {
             const ping = msg.createdTimestamp - interaction.createdTimestamp;
-            await interaction.editReply({ content: `Bot Latency: ${ping}ms\nAPI Latency: ${Math.round(client.ws.ping)}ms` });
+            await interaction.editReply({
+                content: `Bot Latency: ${ping}ms\nAPI Latency: ${Math.round(client.ws.ping)}ms`,
+            });
         });
     }
 }

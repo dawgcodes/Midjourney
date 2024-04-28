77e5e14594120c316b6851c33c44a4accc786de0
TwoDawg <ianblackdavid@gmail.com>
2024-04-28 08:30:45 +0200

    Update Invite.ts

--- src/commands/imagine/Invite.ts
+++ src/commands/imagine/Invite.ts
@@ -1,17 +1,21 @@
-import { CommandInteraction } from "discord.js";
-import { Command, Bot } from "../../structures/index.js";
+import { CommandInteraction } from 'discord.js';
 
+import { Bot, Command } from '../../structures/index.js';
 
 export default class Invite extends Command {
     constructor(client: Bot) {
         super(client, {
             name: 'invite',
+            nameLocalizations: {
+                fr: 'ping',
+            },
             description: {
                 content: '📨 | Get the bot invite link',
                 usage: 'invite',
-                examples: [
-                    'invite',
-                ],
+                examples: ['invite'],
+            },
+            descriptionLocalizations: {
+                fr: '📨 | Afficher le lien d\'invitation.',
             },
             category: 'general',
             permissions: {
@@ -23,7 +27,9 @@ export default class Invite extends Command {
             options: [],
         });
     }
-    async run(client: Bot, interaction: CommandInteraction) {
-        await interaction.reply({ content: `Invite me to your server with this link: [Invite](https://discord.com/oauth2/authorize?client_id=${client.user?.id}&scope=bot%20applications.commands&permissions=8)` });
+    async run(client: Bot, interaction: CommandInteraction): Promise<void> {
+        await interaction.reply({
+            content: `Invite me to your server with this link: [Invite](https://discord.com/oauth2/authorize?client_id=${client.user?.id}&scope=bot%20applications.commands&permissions=8)`,
+        });
     }
 }

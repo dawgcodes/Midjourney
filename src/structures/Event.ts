de1c3c87f690de104bb140a56a91baa963cf4b1d
TwoDawg <ianblackdavid@gmail.com>
2024-04-28 08:29:19 +0200

    Update Event.ts

--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -1,4 +1,9 @@
-import { Bot } from "./index.js";
+import { Bot } from './index.js';
+
+interface EventOptions {
+    name: string;
+    one?: boolean;
+}
 
 export default class Event {
     public client: Bot;
@@ -6,19 +11,16 @@ export default class Event {
     public file: string;
     public name: string;
     public fileName: string;
+
     constructor(client: Bot, file: string, options: EventOptions) {
         this.client = client;
         this.file = file;
-        this.name = options.name
-        this.one = options.one || false;
+        this.name = options.name;
+        this.one = !!options.one;
         this.fileName = file.split('.')[0];
     }
-    public async run(...args: any[]): Promise<void> {
-        return Promise.resolve();
-    }
-}
 
-interface EventOptions {
-    name: string;
-    one?: boolean;
+    public async run(...args: any[]): Promise<void> {
+        return await Promise.resolve();
+    }
 }

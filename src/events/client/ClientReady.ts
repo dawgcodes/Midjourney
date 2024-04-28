c2b38ea9c5222ceab7dc751422df4722b6878f5e
TwoDawg <ianblackdavid@gmail.com>
2024-04-28 08:30:07 +0200

    Update ClientReady.ts

--- src/events/client/ClientReady.ts
+++ src/events/client/ClientReady.ts
@@ -1,7 +1,6 @@
-import { Event, Bot, EventsTypes } from '../../structures/index.js';
-import { ActivityType } from 'discord.js';
+import { Bot, Event, EventsTypes } from '../../structures/index.js';
 
-export default class ClientReady extends Event  {
+export default class ClientReady extends Event {
     constructor(client: Bot, file: string) {
         super(client, file, {
             name: EventsTypes.ClientReady,
@@ -9,10 +8,10 @@ export default class ClientReady extends Event  {
     }
     public async run(): Promise<void> {
         this.client.logger.info(`Connected to Discord as ${this.client.user?.tag}!`);
-        
+
         this.client.user?.setActivity({
-            name: '/imagine',
-            type: ActivityType.Watching
+            name: '/imagine - by dawgcodes',
+            type: 1,
         });
     }
 }

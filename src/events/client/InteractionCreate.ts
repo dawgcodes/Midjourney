2723cf4c3a371601cbf6004a1a6f13f50a7b5d83
TwoDawg <ianblackdavid@gmail.com>
2024-04-28 08:30:17 +0200

    Update InteractionCreate.ts

--- src/events/client/InteractionCreate.ts
+++ src/events/client/InteractionCreate.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction } from 'discord.js';
-import { Event, Bot, EventsTypes } from '../../structures/index.js';
+
+import { Bot, Event, EventsTypes } from '../../structures/index.js';
 
 export default class InteractionCreate extends Event {
     constructor(client: Bot, file: string) {
@@ -14,15 +15,24 @@ export default class InteractionCreate extends Event {
             try {
                 await command.run(this.client, interaction);
             } catch (error) {
-                if (error.message === "Prediction failed: NSFW content detected. Try running it again, or try a different prompt.") {
-                    await interaction![interaction.replied ? 'editReply' : 'reply']({ content: 'NSFW content detected. You can\' generate NSFW images!', ephemeral: true });
+                if (
+                    error.message ===
+                    'Prediction failed: NSFW content detected. Try running it again, or try a different prompt.'
+                ) {
+                    await interaction[interaction.replied ? 'editReply' : 'reply']({
+                        content: 'NSFW content detected. You can\' generate NSFW images!',
+                        ephemeral: true,
+                    });
 
                     return;
                 }
 
                 this.client.logger.error(error);
 
-                await interaction![interaction.replied ? 'editReply' : 'reply']({ content: 'There was an error while executing this command!', ephemeral: true });
+                await interaction[interaction.replied ? 'editReply' : 'reply']({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true,
+                });
             }
         }
     }

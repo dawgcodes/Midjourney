c1bef97264acfa2457255c205364e579f6719b4e
TwoDawg <ianblackdavid@gmail.com>
2024-04-28 08:29:01 +0200

    Update Client.ts

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,50 +1,68 @@
 import {
-    Collection,
-    ClientOptions,
-    Routes,
-    REST,
-    RESTPostAPIChatInputApplicationCommandsJSONBody,
-    PermissionsBitField,
     ApplicationCommandType,
     Client,
+    ClientOptions,
+    Collection,
     EmbedBuilder,
+    PermissionsBitField,
+    REST,
+    RESTPostAPIChatInputApplicationCommandsJSONBody,
+    Routes,
 } from 'discord.js';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import config from '../config.js';
-import Logger from './Logger.js';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import Replicate from 'replicate';
-import { Canvas } from './Canvas.js';
 
+import { Canvas } from './Canvas.js';
+import Logger from './Logger.js';
+import config from '../config.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default class Bot extends Client {
     public config: typeof config;
     public logger = new Logger();
     public commands = new Collection<string, any>();
     private data: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
-    public replicate = new Replicate({
-        auth: config.replicateToken
-    });
+    public replicate: Replicate | null = null;
     public canvas: Canvas = new Canvas();
+
     constructor(options: ClientOptions) {
         super(options);
         this.config = config;
     }
+
     public async start(token: string) {
         this.logger.start('Starting bot...');
-        await this.LoadCommands();
-        await this.LoadEvents();
+        if (!config.replicateToken) {
+            this.logger.error('Replicate token is missing.');
+            return;
+        }
+        this.replicate = new Replicate({
+            auth: config.replicateToken,
+        });
+        if (!this.replicate) {
+            this.logger.error('Failed to initialize Replicate.');
+            return;
+        }
+        this.logger.info('Replicate is initialized.');
+
+        await this.loadCommands();
+        await this.loadEvents();
         await this.login(token);
     }
+
     public embed(): EmbedBuilder {
         return new EmbedBuilder().setColor(this.config.color as any);
     }
-    private async LoadEvents(): Promise<void> {
+
+    private async loadEvents(): Promise<void> {
         const events = fs.readdirSync(path.join(__dirname, '../events'));
         for (const event of events) {
-            const eventFiles = fs.readdirSync(path.join(__dirname, `../events/${event}`)).filter(file => file.endsWith('.js'));
+            const eventFiles = fs
+                .readdirSync(path.join(__dirname, `../events/${event}`))
+                .filter(file => file.endsWith('.js'));
             for (const file of eventFiles) {
                 const eventFile = (await import(`../events/${event}/${file}`)).default;
                 const eventClass = new eventFile(this, file);
@@ -52,44 +70,47 @@ export default class Bot extends Client {
             }
         }
     }
-    private async LoadCommands(): Promise<void> {
+
+    private async loadCommands(): Promise<void> {
         const commandsPath = fs.readdirSync(path.join(__dirname, '../commands'));
         for (const commandPath of commandsPath) {
-            const commandFiles = fs.readdirSync(path.join(__dirname, `../commands/${commandPath}`)).filter(file => file.endsWith('.js'));
+            const commandFiles = fs
+                .readdirSync(path.join(__dirname, `../commands/${commandPath}`))
+                .filter(file => file.endsWith('.js'));
             for (const file of commandFiles) {
                 const commandFile = (await import(`../commands/${commandPath}/${file}`)).default;
                 const command = new commandFile(this, file);
                 this.commands.set(command.name, command);
-                const data = {
+                const data: RESTPostAPIChatInputApplicationCommandsJSONBody = {
                     name: command.name,
                     description: command.description.content,
                     type: ApplicationCommandType.ChatInput,
-                    options: command.options ? command.options : null,
-                    name_localizations: command.nameLocalizations ? command.nameLocalizations : null,
-                    description_localizations: command.descriptionLocalizations ? command.descriptionLocalizations : null,
-                    default_member_permissions: command.permissions.user.length > 0 ? command.permissions.user : null,
+                    options: command.options || null,
+                    name_localizations: command.nameLocalizations || null,
+                    description_localizations: command.descriptionLocalizations || null,
+                    default_member_permissions:
+                        command.permissions.user.length > 0 ? command.permissions.user : null,
                 };
                 if (command.permissions.user.length > 0) {
                     const permissionValue = PermissionsBitField.resolve(command.permissions.user);
-                    if (typeof permissionValue === 'bigint') {
-                        data.default_member_permissions = permissionValue.toString();
-                    } else {
-                        data.default_member_permissions = permissionValue;
-                    }
+                    data.default_member_permissions =
+                        typeof permissionValue === 'bigint'
+                            ? permissionValue.toString()
+                            : permissionValue;
                 }
-                const json = JSON.stringify(data);
-                this.data.push(JSON.parse(json));
+                this.data.push(data);
             }
-            this.once('ready', async () => {
-                const applicationCommands = Routes.applicationCommands(this.config.clientId ?? '');
-                try {
-                    const rest = new REST({ version: '9' }).setToken(this.config.token ?? '');
-                    await rest.put(applicationCommands, { body: this.data });
-                    this.logger.info(`Successfully loaded slash commands!`);
-                } catch (error) {
-                    this.logger.error(error);
-                }
-            });
         }
+
+        this.once('ready', async () => {
+            const applicationCommands = Routes.applicationCommands(this.config.clientId ?? '');
+            try {
+                const rest = new REST({ version: '10' }).setToken(this.config.token ?? '');
+                await rest.put(applicationCommands, { body: this.data });
+                this.logger.info(`Successfully loaded slash commands!`);
+            } catch (error) {
+                this.logger.error(error);
+            }
+        });
     }
 }

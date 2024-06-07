d3c013a68097e193b608fdf04fc42927a239ca25
TwoDawg <ianblackdavid@gmail.com>
2024-06-07 21:45:32 +0200

    Delete src directory

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-export default {
-    token: process.env.TOKEN,
-    clientId: process.env.CLIENT_ID,
-    color: process.env.COLOR,
-    replicateToken: process.env.REPLICATE_TOKEN,
-    model: process.env.MODEL as any,
-};
--- src/structures/index.ts
+++
@@ -1,7 +0,0 @@
-import { Events } from 'discord.js';
-
-import Bot from './Client.js';
-import Command from './Command.js';
-import Event from './Event.js';
-
-export { Bot, Event, Command, Events as EventsTypes };

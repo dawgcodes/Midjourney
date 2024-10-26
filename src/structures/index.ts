16ca8ca6702857321308087946be394293487faa
TwoDawg <ianblackdavid@gmail.com>
2024-10-26 05:05:16 +0200

    Delete src directory

--- src/structures/index.ts
+++
@@ -1,8 +0,0 @@
-import { Events } from "discord.js";
-
-import Bot from "./Client.js";
-import Command from "./Command.js";
-import Context from "./Context.js";
-import Event from "./Event.js";
-
-export { Bot, Event, Command, Events, Context };
